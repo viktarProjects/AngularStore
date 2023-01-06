3860ad55ec62e5ce6f56490034f41d30f2bedeaf
viktar33 <viktar.varabei@mail.ru>
2023-01-06 12:12:55 +0300

    1)

---
+++ AngularStore.Database/Data/StoreContext.cs
@@ -0,0 +1,19 @@
+﻿using AngularStore.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngularStore.Database.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ AngularStore.WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AngularStore.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "It will be list of products";
+        }
+
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return $"It will be one product № {id}";
+        }
+    }
+}
